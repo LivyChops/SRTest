bd81c3a0554964b66e8605ed7537aeb856978319
Livy <pgbmobile@gmail.com>
2015-08-07 10:00:59 +1000

    Piotr's suggested Fix worked of course :) + injected IConnectionManager

--- Hubs/MessagesHub.cs
+++ Hubs/MessagesHub.cs
@@ -1,25 +1,27 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
-using Orchard;
+using Microsoft.AspNet.SignalR.Infrastructure;
 
 namespace SRTest.SignalR.Hubs
-{
+{ 
     [HubName("messagesHub")] //- found that case "M" effected whether this worked [I assume it has to be the same as JS].
     public class MessagesHub : Hub {
+        private static IConnectionManager _connectionManager;
 
-        //private readonly IOrchardServices services;
-
-        //public MessagesHub(IOrchardServices services)
-        //{
-        //    this.services = services;
-        //}
+        public MessagesHub(IConnectionManager connectionManager)
+        {
+            _connectionManager = connectionManager;
+        }
 
         [HubMethodName("SendMessages")] //changed casing ("sendMessages")]
-        public static void SendMessages() //static
+        public static void SendMessages()
         {
-            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<MessagesHub>();
+            IHubContext context = _connectionManager.GetHubContext<MessagesHub>();
             context.Clients.All.updateMessages();
-            //Clients.All.updateMessages();
+
+            //Old code: Does not work, you have to inject IConnectionManager
+            //IHubContext context = GlobalHost.ConnectionManager.GetHubContext<MessagesHub>();
+            //context.Clients.All.updateMessages();
         }
     }
 }
